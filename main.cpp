61dbcec83c482a1df42b1f82a0ae979cbc2a93fe
Ychixm <yochix.M@hotmail.fr>
2020-10-12 19:22:05 +0200

    Initial commit.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "situation/Instance.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numberOfShipping = 10;
+    int mapSize = 100;
+    Instance rand(numberOfShipping,mapSize);
+    std::cout << rand << std::endl;
+    rand.generateDistanceMatrix();
+    rand.showDestinationMatrix();
     return 0;
 }
